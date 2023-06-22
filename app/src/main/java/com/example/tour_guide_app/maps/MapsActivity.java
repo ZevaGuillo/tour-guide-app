4d94d84f6b912ca4db506409fb0ef868edcfb721
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-06-22 15:16:53 -0500

    add map

---
+++ app/src/main/java/com/example/tour_guide_app/maps/MapsActivity.java
@@ -0,0 +1,30 @@
+package com.example.tour_guide_app.maps;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tour_guide_app.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
