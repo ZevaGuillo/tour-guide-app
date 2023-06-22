fb8a5becf4c40229f35bfba8ad2014e003514037
Ricardofaky <57514274+Ricardofaky@users.noreply.github.com>
2023-06-22 14:35:34 -0500

    Cambios en comments

---
+++ app/src/main/java/com/example/tour_guide_app/comments/CommentsActivity.java
@@ -0,0 +1,16 @@
+package com.example.tour_guide_app.comments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tour_guide_app.R;
+
+public class CommentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comments);
+    }
+}
